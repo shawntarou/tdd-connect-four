68cafaaa6e2fb05ef65cbf33028808a28fe16680
shawn <173310929+shawntarou@users.noreply.github.com>
2025-10-12 18:25:34 -0700

    Add get_player_name method

--- main.rb
+++ main.rb
@@ -46,19 +46,39 @@ class GameBoard
 end
 
 class Player
-  def initalize(name, piece)
+  attr_accessor :name, :player_num
+
+  def initialize(name, piece, player_num)
     @name = name
     @piece = piece
+    @player_num = player_num
   end
 end
 
 class ConnectFourGame
-  def initalize(board, player_one, player_two)
-    @board = board
-    @player_one = player_one
-    @player_two = player_two
+  def initalize
+    @board = GameBoard.new
+    @player_one = Player.new('Player 1', '㉧', 1)
+    @player_two = Player.new('Player 2', '㉨', 2)
   end
 
+
+  def start_game
+
+  end
+  
+  private
+  
+  def get_player_name(player)
+    puts "Player #{player.player_num} Name:"
+    puts "Press Enter For Default Name - \"#{player.name}\""
+    print '> '
+    entered_name = gets.chomp
+    unless entered_name == ''
+      player.name = entered_name
+    end
+  end
+  
   def valid_move?(column_num)
     @board.reverse_each.with_index do |row, index|
       if row[board_column_num] == "*" 
@@ -70,13 +90,22 @@ class ConnectFourGame
   end
 end
 
-puts '㉧ ㉨'
-puts '㉧ ㉨'
-puts '㉧ ㉨'
-puts '㉧ ㉨'
-puts '㉧ ㉨'
-puts '㉧ ㉨'
-puts
 
-game_board = GameBoard.new
+
+# puts '㉧ ㉨'
+# puts '㉧ ㉨'
+# puts '㉧ ㉨'
+# puts '㉧ ㉨'
+# puts '㉧ ㉨'
+# puts '㉧ ㉨'
+# puts
+
+# game_board = GameBoard.new
 # game_board.print_board
+
+player_one = Player.new('George', '%', 1)
+game = ConnectFourGame.new
+game.get_player_name(player_one)
+puts
+puts player_one.name
+
